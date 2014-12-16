ba90c5b8b9f6e6b0bb35924a068037e0c7292a14
hackreactor-robot <phillip+hackreactor-robot@hackreactor.com>
2014-12-16 07:45:56 +0000

    (init)

---
+++ web/basic-server.js
@@ -0,0 +1,13 @@
+var http = require("http");
+var handler = require("./request-handler");
+var initialize = require("./initialize.js");
+
+// Why do you think we have this here?
+// HINT:It has to do with what's in .gitignore
+initialize();
+
+var port = 8080;
+var ip = "127.0.0.1";
+var server = http.createServer(handler.handleRequest);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
